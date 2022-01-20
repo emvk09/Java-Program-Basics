0292d2c6ca15547da2207018626b695e74522682
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-01-21 01:27:49 +0530

    Update StackOperations.java

--- StackOperations.java
+++ StackOperations.java
@@ -1,51 +1,67 @@
 import java.util.*;
 
-class Operations
+class Stack
 {
     Scanner sc= new Scanner(System.in);
     int Maxsize;
     int top;
-    int stack[];
-    Operations(int size)
+    int arr[];
+    Stack(int size)
     {
         top= -1;
-        stack= new int[size];
+        arr= new int[size];
+		// Maxsize= size;
     }
 
-    void push()
+	/* isEmpty() and isFull() functions are not necessarily needed. We can simply write 
+	the return results in the if statements of push() and pull() functions. */
+
+	boolean isFull(){
+		return (top == Maxsize- 1);
+	}
+
+	boolean isEmpty(){
+		return (top ==- 1);
+	}
+
+    void push(int y)
     {
-	    int x;
-	    if (top == Maxsize- 1)
-		    System.out.println("The stack is full.\n");
+	    if (isFull())
+		    System.out.println("Overflow !!\n");
 	    else
 	    {
-		    System.out.println("Enter the number: ");
-		    x= sc.nextInt();
 		    top++;
-		    stack[top]= x;
+		    arr[top]= y;
+			System.out.format("Inserted element: %d\n", y);
 	    }
     }
 
     void pop()
     {
-	    if (top ==- 1)
-            System.out.println("The stack is empty.\n");
+	    if (isEmpty())
+            System.out.println("Underflow !!\n");
 	    else
 	    {
-		    System.out.format("Popped element: %d\n", stack[top]);
+		    System.out.format("Popped element: %d\n", arr[top]);
 		    top--;
 	    }
     }
 
+	void peek()
+	{
+		if ( ! isEmpty())
+			System.out.format("Peeked element: %d\n", arr[top]);
+	}
+
     void display()
     {
-	    if (top ==- 1)
+	    if (isEmpty())
             System.out.println("The stack is empty.\n");
 	    else
 	    {
 		    System.out.println("Stack:");
 		    for(int i= top; i>= 0; i--)
-                System.out.format("%d\n", stack[i]);
+                System.out.format("%d\n", arr[i]);
 	    }
     }
 }
@@ -55,7 +71,7 @@ public class StackOperations
 {
     public static void main(String arr[])
     {
-        int option, size;
+        int option, size, x;
         Scanner sc= new Scanner (System.in);
         System.out.println("\nEnter the maximum size of stack:");
         size= sc.nextInt();
@@ -70,24 +86,28 @@ public class StackOperations
 					  These values can be assigned as a statement in the constructor body or passed as arguments.*/ 
 
 
-        Operations object= new Operations(size);
+        Stack object= new Stack(size);
         object.Maxsize= size;
 	    while(true)
 	    {
-		    System.out.println("\nOption 1: Push\nOption 2: Pop\nOption 3: display\nOption 4: Exit\n");
-		    System.out.println("Enter the option number: ");
+		    System.out.println("\nOption 1: Push\nOption 2: Pop\nOption 3: peek\nOption 4: display\nOption 5: Exit");
+		    System.out.print("Enter the option number: ");
 		    option= sc.nextInt();
 		    switch(option)
 		    {
-			    case 1: object.push();
-					break;
+			    case 1: System.out.print("Enter the number: ");
+						x= sc.nextInt();
+						object.push(x);
+						break;
 			    case 2: object.pop();
-					break;
-			    case 3: object.display();
-					break;
-			    case 4: System.exit(0);
+						break;
+				case 3: object.peek();
+						break;
+			    case 4: object.display();
+						break;
+			    case 5: System.exit(0);
 			    default:
-                    System.out.println("Invalid option. Try again\n");
+                    	System.out.println("Invalid option. Try again\n");
 		    }
 	    }
     }    
