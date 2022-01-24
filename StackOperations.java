422a71604a26e1911fd7a10d197d4121522853c6
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-01-24 10:38:44 +0530

    Update StackOperations.java

--- StackOperations.java
+++ StackOperations.java
@@ -1,5 +1,52 @@
 import java.util.*;
 
+public class StackOperations 
+{
+    public static void main(String arr[])
+    {
+        int option, size, x;
+        Scanner sc= new Scanner (System.in);
+        System.out.println("\nEnter the maximum size of stack:");
+        size= sc.nextInt();
+
+        /* Here We are assigning the value of size variable to both the constructor ( Operation() )
+        and the instance variable called Maxsize.
+        We are inputing the value to the constructor because we cannot initialize the values in the 
+        class Operations. We can only declare in the class Operations. Same is also for the case of arrays.
+        eg: top= -1 ; This Initialization statement cannot be written in the class Operation alone.
+                      Only Declaration (int top;) can be done in that class
+                      The value must be initialized only inside a constructor. 
+					  These values can be assigned as a statement in the constructor body or passed as arguments.*/ 
+
+
+        Stack object= new Stack(size);
+        object.Maxsize= size;
+		
+	    while(true)
+	    {
+		    System.out.println("\nOption 1: Push\nOption 2: Pop\nOption 3: peek\nOption 4: display\nOption 5: Exit");
+		    System.out.print("Enter the option number: ");
+		    option= sc.nextInt();
+		    switch(option)
+		    {
+			    case 1: System.out.print("Enter the number: ");
+						x= sc.nextInt();
+						object.push(x);
+						break;
+			    case 2: object.pop();
+						break;
+				case 3: object.peek();
+						break;
+			    case 4: object.display();
+						break;
+			    case 5: System.exit(0);
+			    default:
+                    	System.out.println("Invalid option. Try again\n");
+		    }
+	    }
+    }    
+}
+
 class Stack
 {
     Scanner sc= new Scanner(System.in);
@@ -64,51 +111,4 @@ class Stack
                 System.out.format("%d\n", arr[i]);
 	    }
     }
-}
-
-
-public class StackOperations 
-{
-    public static void main(String arr[])
-    {
-        int option, size, x;
-        Scanner sc= new Scanner (System.in);
-        System.out.println("\nEnter the maximum size of stack:");
-        size= sc.nextInt();
-
-        /* Here We are assigning the value of size variable to both the constructor ( Operation() )
-        and the instance variable called Maxsize.
-        We are inputing the value to the constructor because we cannot initialize the values in the 
-        class Operations. We can only declare in the class Operations. Same is also for the case of arrays.
-        eg: top= -1 ; This Initialization statement cannot be written in the class Operation alone.
-                      Only Declaration (int top;) can be done in that class
-                      The value must be initialized only inside a constructor. 
-					  These values can be assigned as a statement in the constructor body or passed as arguments.*/ 
-
-
-        Stack object= new Stack(size);
-        object.Maxsize= size;
-	    while(true)
-	    {
-		    System.out.println("\nOption 1: Push\nOption 2: Pop\nOption 3: peek\nOption 4: display\nOption 5: Exit");
-		    System.out.print("Enter the option number: ");
-		    option= sc.nextInt();
-		    switch(option)
-		    {
-			    case 1: System.out.print("Enter the number: ");
-						x= sc.nextInt();
-						object.push(x);
-						break;
-			    case 2: object.pop();
-						break;
-				case 3: object.peek();
-						break;
-			    case 4: object.display();
-						break;
-			    case 5: System.exit(0);
-			    default:
-                    	System.out.println("Invalid option. Try again\n");
-		    }
-	    }
-    }    
 }
