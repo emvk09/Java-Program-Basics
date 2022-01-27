78e4b8ae695579556cd378fb1db47d420438f539
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-01-27 12:06:26 +0530

    Update FactorialCmd.java

--- FactorialCmd.java
+++ FactorialCmd.java
@@ -3,7 +3,7 @@ public class FactorialCmd
     public static void main (String arr[])
     {
         int a, i,fact= 1;
-        a=  Integer.parseInt(arr[0]);
+        a=  Integer.parseInt(arr[0]); //The parseInt function converts its first argument to a string, parses that string, then returns an integer
         for(i= 1; i<= a; i++)
             fact= fact* i;
         System.out.println("The factorial is : " + fact);
