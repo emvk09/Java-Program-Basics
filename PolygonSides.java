87260ab77696e1dc8f01f6c3570cce901ce9e952
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-01-31 10:34:13 +0530

    Update PolygonSides.java

--- PolygonSides.java
+++ PolygonSides.java
@@ -2,7 +2,7 @@
 Provide three classes named Rectangle, Triangle, and Hexagon such that each one of the classes extends the class Shape. 
 Each one of the classes contains only the method numberOfSides( ) that shows the number of sides in the given geometrical structures.*/
 
-// Abstract classes cannot be instantiated.
+// Abstract classes cannot be instantiated but its subclasses can be.
 // It can have constructors and static methods also.
 // Abstract classes are used to hide the working part (or the important) of a of a program from the consumer or end user or another programmers
 // In Abstract classes, we use only the declaration of methods, thus hiding the method implementation or the method defintion   
