11bc4403ca81afa16e9d8b7cc1221f1895095958
emvk09 <84259272+emvk09@users.noreply.github.com>
2022-02-20 00:31:16 +0530

    Update Result.java

--- Result.java
+++ Result.java
@@ -3,7 +3,10 @@ package studpack;
 interface Sports
 {
     int grade = 100;
+    void displayName();
+    void displayRoll();
     void displayGrade();
+    void displayTotal();
 }
 
 class Student
@@ -12,52 +15,49 @@ class Student
     int rollno;
     float mark1, mark2, mark3;
 
-    Student()
+    Student(String n, int r, float m1, float m2, float m3)
     {
-        name= "NONE";
-        rollno= 0;
-        mark1= mark2= mark3= 0;
-    }
-
-    Student(String name, int rollno, float mark1, float mark2, float mark3)
-    {
-        this.name= name;
-        this.rollno= rollno;
-        this.mark1= mark1;
-        this.mark2= mark2;
-        this.mark3= mark3;
+        this.name= n;
+        this.rollno= r;
+        this.mark1= m1;
+        this.mark2= m2;
+        this.mark3= m3;
     }
 }
 
 public class Result extends Student implements Sports 
 {
     float total;
+    
+    // All the parameters from the main program is passed to Result constructor
+    public Result(String name, int rollNo, float mark1, float mark2, float mark3) 
+    {
+        // super calls the constructor with same parameters from the parent class.
+        // Suppose if the parent class had only one constructor with no parameters at all, then there arises error 
+        super(name, rollNo, mark1, mark2, mark3);
+        total= mark1 + mark2 + mark3;
+    }
+
+    public void displayTotal()
+    {
+        displayName();
+        displayRoll();
+        displayGrade();
+        System.out.println("Total obtained = " + total + "/300\n");
+    }
+
+    public void displayName()
+    {
+        System.out.println("Name = " + name);
+    }
+
+    public void displayRoll()
+    {
+        System.out.println("Roll no = " + rollno);
+    }
+
     public void displayGrade()
     {
         System.out.println("Sports Grade = " + grade);
     }
-    public void displayTotal()
-    {
-        System.out.println("Roll No = " + rollno);
-        displayGrade();
-        System.out.println("Total obtained = " + total + "/400\n");
-    }
-    public Result()
-    {
-        super();
-        total= 0;
-    }
-    public Result(String name, int rollNo, float mark1, float mark2, float mark3)
-    {
-        super(name, rollNo, mark1, mark2, mark3);
-        total= mark1 + mark2 + mark3 + grade;
-    }
 }
-
-
-
-
-
-
-
-
